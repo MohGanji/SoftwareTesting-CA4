a1c488b13422c7be14ac4d5fd6a1e62d08c3e5a2
amir.karimi6610 <iamthetallboy>
2018-11-25 23:49:01 +0330

    finished smoke tests

--- src/test/java/SmartHomeCntrlTest.java
+++ src/test/java/SmartHomeCntrlTest.java
@@ -1,8 +1,10 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SmartHomeCntrlTest {
+
     @Test
     public void test1() {
         SecurityConfig securityConfig = new SecurityConfig(1, SecurityMode.Normal_Mode);
@@ -12,5 +14,161 @@ public class SmartHomeCntrlTest {
         smartHomeController.gas(2);
         assertEquals(new HomeState(Windows.Close, Doors.Close, Lights.NormalDay, Alarm.Off),
                 smartHomeController.getHomeState());
+    }
+
+    @Test
+    public void smoke_test1() {  // TTT
+        SecurityConfig securityConfig = new SecurityConfig(1, SecurityMode.Normal_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Day);
+        smartHomeController.smoke(0);
+        assertTrue(new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.Off).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test2() {  // FTT
+        SecurityConfig securityConfig = new SecurityConfig(1, SecurityMode.Thief_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Day);
+        smartHomeController.smoke(0);
+        assertTrue(new HomeState(Windows.Close, Doors.Close, Lights.Off, Alarm.Off).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test3() {  // TFT
+        SecurityConfig securityConfig = new SecurityConfig(1, SecurityMode.Normal_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Night);
+        smartHomeController.smoke(0);
+        assertTrue(new HomeState(Windows.Close, Doors.Close, Lights.Off, Alarm.Off).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test4() {  // TTF
+        SecurityConfig securityConfig = new SecurityConfig(1, SecurityMode.Normal_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Day);
+        smartHomeController.smoke(2);
+        assertTrue(new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.Off).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test5() {  // FTF
+        SecurityConfig securityConfig = new SecurityConfig(1, SecurityMode.Thief_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Day);
+        smartHomeController.smoke(2);
+        assertTrue(new HomeState(Windows.Close, Doors.Close, Lights.Off, Alarm.Off).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test6() {  // TFF
+        SecurityConfig securityConfig = new SecurityConfig(1, SecurityMode.Normal_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Night);
+        smartHomeController.smoke(2);
+        assertTrue(new HomeState(Windows.Close, Doors.Close, Lights.Off, Alarm.Off).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test7() {  // FF
+        SecurityConfig securityConfig = new SecurityConfig(1, SecurityMode.Vacation_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Night);
+        smartHomeController.smoke(2);
+        assertTrue(new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.On).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    // part 2
+
+    @Test
+    public void smoke_test8() {  // FTTFT
+        SecurityConfig securityConfig = new SecurityConfig(4, SecurityMode.Thief_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Night);
+        smartHomeController.smoke(2);
+        assertTrue(new HomeState(Windows.Open, Doors.Open, Lights.Emergency, Alarm.Off).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test9() {  // FFTFT
+        SecurityConfig securityConfig = new SecurityConfig(4, SecurityMode.Vacation_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Night);
+        smartHomeController.smoke(2);
+        assertTrue(new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.On).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test10(){  // FTFFT
+        SecurityConfig securityConfig = new SecurityConfig(2, SecurityMode.Thief_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Night);
+        smartHomeController.smoke(0);
+        assertTrue(new HomeState(Windows.Close, Doors.Close, Lights.Off, Alarm.Off).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test11(){  // FTTTT
+        SecurityConfig securityConfig = new SecurityConfig(4, SecurityMode.Thief_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Day);
+        smartHomeController.smoke(2);
+        assertTrue(new HomeState(Windows.Close, Doors.Close, Lights.Off, Alarm.Off).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test12() {  // FTTFF
+        SecurityConfig securityConfig = new SecurityConfig(4, SecurityMode.Thief_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Night);
+        smartHomeController.smoke(0);
+        assertTrue(new HomeState(Windows.Close, Doors.Close, Lights.Off, Alarm.Off).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test13() {  // T
+        SecurityConfig securityConfig = new SecurityConfig(0, SecurityMode.Vacation_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Night);
+        smartHomeController.smoke(0);
+        assertTrue(new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.On).equals
+                (smartHomeController.getHomeState()));
+    }
+
+    @Test
+    public void smoke_test14() {  // F
+        SecurityConfig securityConfig = new SecurityConfig(0, SecurityMode.Normal_Mode);
+        EnvState envState = new EnvState(0, 0, 0);
+        SmartHomeController smartHomeController =
+                new SmartHomeController(securityConfig, envState, Time.Night);
+        smartHomeController.smoke(0);
+        assertTrue(new HomeState(Windows.Close, Doors.Close, Lights.Off, Alarm.Off).equals
+                (smartHomeController.getHomeState()));
     }
 }
